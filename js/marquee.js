072f486f708a274a77a6c1220252303db66dd76b
Jacques <janneslambrecht@hotmail.com>
2024-05-17 11:50:44 +0200

    update

---
+++ js/marquee.js
@@ -0,0 +1,6 @@
+import marquee from 'https://cdn.jsdelivr.net/npm/vanilla-marquee/dist/vanilla-marquee.js';
+
+new marquee( document.getElementById( 'marquee' ), {
+  speed:      150,
+  pauseOnHover: true
+});
